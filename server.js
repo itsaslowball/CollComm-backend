3aa9da2f1fcfdd5773c5544157679cb4543c8f37
Priyanshu <priya14anshu@gmail.com>
2023-02-13 00:37:54 +0530

    .

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ const server = app.listen(PORT, console.log(`server running on port ${PORT}`));
 const io = require('socket.io')(server, {
     pingTimeout: 60000,
     cors: {
-        origin: "http://localhost:3000",
+        origin: ["http://localhost:3000","https://colcom.onrender.com"]
     }
 });
 
